d106c415ec3c5db502e4f45bc5ea63c8e7ad7718
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-12-19 20:05:07 +0700

    menambahkan StatusUiSiswa untuk state UI

---
+++ app/src/main/java/com/example/questapi_006/viewmodel/HomeViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.questapi_006.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_006.modeldata.DataSiswa
+import com.example.questapi_006.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val Siswa: List<DataSiswa>) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+
