c552cb1a3506ff084b32da4e40e10b020a53a892
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-12-19 21:27:35 +0700

    menyesuaikan repositori data siswa agar tdk eror

--- app/src/main/java/com/example/questapi_006/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_006/repositori/RepositoryDataSiswa.kt
@@ -4,14 +4,15 @@ import com.example.questapi_006.apiservice.ServiceApiSiswa
 import com.example.questapi_006.modeldata.DataSiswa
 import retrofit2.Response
 
-interface RepositoryDataSiswa {
+interface RepositoryDataSiswa{
     suspend fun getDataSiswa(): List<DataSiswa>
-    suspend fun postDataSiswa(dataSiswa : DataSiswa) : retrofit2.Response<Void>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ):RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+
 }
