88b65ab0803616c24d13da45b982c4249fd8ee2f
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-12-26 14:41:36 +0700

    untuk mengelola logika data dan fitur edit

--- app/src/main/java/com/example/questapi_006/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_006/viewmodel/EditViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.questapi_006.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_006.modeldata.DetailSiswa
+import com.example.questapi_006.modeldata.UIStateSiswa
+import com.example.questapi_006.modeldata.toDataSiswa
+import com.example.questapi_006.modeldata.toUiStateSiswa
+import com.example.questapi_006.repositori.RepositoryDataSiswa
+import com.example.questapi_006.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
